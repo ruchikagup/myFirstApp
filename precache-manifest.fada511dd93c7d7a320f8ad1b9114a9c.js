81960f0b62e35919b0661e9349c943177ee96164
Ruchika <ruchikaster@gmail.com>
2020-07-05 14:28:21 +0200

    Updates

---
+++ precache-manifest.fada511dd93c7d7a320f8ad1b9114a9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a5c00fab6dd96afb3f503ca87804ca",
+    "url": "/myFirstApp/index.html"
+  },
+  {
+    "revision": "a5f310e91c45f0e3bc75",
+    "url": "/myFirstApp/static/css/main.60bc5739.chunk.css"
+  },
+  {
+    "revision": "bcfa43311016a6a9dcbe",
+    "url": "/myFirstApp/static/js/2.543d7b69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myFirstApp/static/js/2.543d7b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f310e91c45f0e3bc75",
+    "url": "/myFirstApp/static/js/main.85fdf19f.chunk.js"
+  },
+  {
+    "revision": "1a4472a718cc59a4c0f7",
+    "url": "/myFirstApp/static/js/runtime-main.d3951bc8.js"
+  }
+]);
